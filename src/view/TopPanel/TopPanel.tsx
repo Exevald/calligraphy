21f901741e71504250123354c1e72f069c42588c
Konstantin Kalinin <kalinin.konst.official@gmail.com>
2024-12-02 21:03:05 +0300

    Написал прототип генерации текста по пропсам

--- src/view/TopPanel/TopPanel.tsx
+++ src/view/TopPanel/TopPanel.tsx
@@ -1,3 +1,5 @@
+
+
 const TopPanel = () => {
     return (
         <div>
--- src/view/pages/EditorPage/EditorPage.tsx
+++ src/view/pages/EditorPage/EditorPage.tsx
@@ -1,9 +1,74 @@
-import TopPanel from '../../TopPanel/TopPanel'
+import React, {useState} from "react";
+import HandwritingCanvas from "../../../HandwritingCanvas";
+import {LetterStyle} from "../../../core/utility";
+
 
 const EditorPage = () => {
+
+    const [text, setText] = useState('Привет');
+    const [style, setStyle] = useState<LetterStyle>('solid');
+
+    const handleStyleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setStyle(event.target.value as LetterStyle);
+    };
+
     return (
         <div>
-            <TopPanel/>
+            <h1>Генератор прописей</h1>
+            <input
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Введите текст"
+            />
+            <div>
+                <label>
+                    <input
+                        type="radio"
+                        value="solid"
+                        checked={style === 'solid'}
+                        onChange={handleStyleChange}
+                    />
+                    Сплошной
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="dashed"
+                        checked={style === 'dashed'}
+                        onChange={handleStyleChange}
+                    />
+                    Пунктир
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="dotted"
+                        checked={style === 'dotted'}
+                        onChange={handleStyleChange}
+                    />
+                    Точки
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="traced"
+                        checked={style === 'traced'}
+                        onChange={handleStyleChange}
+                    />
+                    Обводка (с тенью)
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="outlined"
+                        checked={style === 'outlined'}
+                        onChange={handleStyleChange}
+                    />
+                    Контур
+                </label>
+            </div>
+            <HandwritingCanvas text={text} style={style} />
         </div>
     )
 }
